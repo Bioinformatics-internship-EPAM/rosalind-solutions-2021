d6fd9d5c0ce300f27647d0b926d077c25ac422ef
Anzhelika <lisenok5701@yandex.ru>
2021-02-04 16:04:37 +0300

    Test commit

---
+++ Anzhelika_Osipova/DNA/src/DNA.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.nio.file.*;
+
+public class DNA 
+{
+
+	public static void main(String[] args)
+	{
+		Path fin = Path.of("data.txt");
+		Path fout = Path.of("out.txt");
+		int[] countNuc = {0, 0, 0, 0};
+		try 
+		{
+			String data = Files.readString(fin);
+			for(int i = 0; i < data.length(); i++)
+			{
+				switch(data.charAt(i))
+				{
+					case ('A'):
+						countNuc[0]++;
+						break;
+					case ('C'):
+						countNuc[1]++;
+						break;
+					case ('G'):
+						countNuc[2]++;
+						break;
+					case ('T'):
+						countNuc[3]++;
+						break;					
+				}
+			}
+			System.out.println(countNuc[0] + " " + countNuc[1] + " " + countNuc[2] + " " + countNuc[3]);
+			Files.writeString(fout, countNuc[0] + " " + countNuc[1] + " " + countNuc[2] + " " + countNuc[3]);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
