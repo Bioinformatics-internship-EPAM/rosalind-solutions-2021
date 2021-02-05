354e206e40e639be87a678af68b3550cf0ab948a
Ivan Selivanov <ivanselivanov1999@gmail.com>
2021-02-05 10:05:33 +0300

    change regex to substring search

--- ivan_selivanov/SUBS.scala
+++ ivan_selivanov/SUBS.scala
@@ -2,6 +2,9 @@ import scala.io.StdIn.readLine
 
 object SUBS extends App {
   val (text, pattern) = (readLine(), readLine())
-  val matches = s"(?=($pattern))".r.findAllMatchIn(text)
-  println(matches.map(_.start + 1).mkString(" "))
+  val indexes_list =
+    for (index <- 0 until text.length - pattern.length + 1
+         if text.substring(index, index + pattern.length) == pattern)
+      yield index + 1
+  print(indexes_list.mkString(" "))
 }
