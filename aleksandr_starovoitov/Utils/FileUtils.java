6877cd0f5a7c7011b3dcc32e4970a42676675db3
Aleksandr Starovoitov <starovoytov.it@mail.ru>
2021-02-16 23:48:44 +0300

    Add variable for Paths.get argument

--- aleksandr_starovoitov/Utils/FileUtils.java
+++ aleksandr_starovoitov/Utils/FileUtils.java
@@ -5,7 +5,8 @@ import java.util.List;
 
 public class FileUtils {
     public static List<String> readFromFile(String fileName) throws Exception {
-        var fullPath = Paths.get(FileUtils.class.getResource(fileName).getPath().substring(1));
+        var path = FileUtils.class.getResource(fileName).getPath().substring(1);
+        var fullPath = Paths.get(path);
         return Files.readAllLines(fullPath);
     }
 
