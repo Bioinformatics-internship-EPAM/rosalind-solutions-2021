68d226d2cbb4921be72a82f21c43657708bfe3c9
Vladislav Nevzorov <v.nevzorov@corp.vk.com>
2021-02-23 16:48:36 +0300

    updated code

--- vladislav_nevzorov/1_CountingDNANucleotides/CountingDNANucleotides.java
+++ vladislav_nevzorov/1_CountingDNANucleotides/CountingDNANucleotides.java
@@ -3,29 +3,27 @@ package my.rosalind;
 import java.util.HashMap;
 
 public class CountingDNANucleotides {
-    private char[] getAlhpabet() {
-        return new char[] {'A', 'C', 'G', 'T'};
-    }
+    private char[] getAlhpabet() { return new char[] {'A', 'C', 'G', 'T'}; }
 
     public int[] exec(String dataset) {
-        char[] alphabet = getAlhpabet();
-        HashMap<Character, Integer> map = new HashMap<>();
+        var alphabet = getAlhpabet();
+        var map = new HashMap<Character, Integer>();
 
         for (char symbol : alphabet) {
             map.put(symbol, 0);
         }
 
-        for (char symbol : dataset.toCharArray()) {
-            Integer value = map.get(symbol);
+        for (var symbol : dataset.toCharArray()) {
+            var value = map.get(symbol);
             if (value == null) {
                 continue;
             }
             map.replace(symbol, ++value);
         }
 
-        int[] result = new int[alphabet.length];
+        var result = new int[alphabet.length];
         for (int i = 0; i < alphabet.length; i++) {
-            char symbol = alphabet[i];
+            var symbol = alphabet[i];
             result[i] = map.get(symbol);
         }
 
