31417ade085fbdbec6f76a6e982cd4a52c87054c
dadlex <alex15051997@yandex.ru>
2021-02-25 00:34:58 +0300

    [Task 5] Add protein string creation out of rna [Task 6] Add protein mass evaluation

---
+++ alexander_aphonin/proteinMassCalculation/Task6.java
@@ -0,0 +1,41 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Task6 {
+    public static Map<String, Double> monoisotopicMasses;
+
+    static {
+        monoisotopicMasses = new HashMap<>();
+        monoisotopicMasses.put("A", 71.03711);
+        monoisotopicMasses.put("C", 103.00919);
+        monoisotopicMasses.put("D", 115.02694);
+        monoisotopicMasses.put("E", 129.04259);
+        monoisotopicMasses.put("F", 147.06841);
+        monoisotopicMasses.put("G", 57.02146);
+        monoisotopicMasses.put("H", 137.05891);
+        monoisotopicMasses.put("I", 113.08406);
+        monoisotopicMasses.put("K", 128.09496);
+        monoisotopicMasses.put("L", 113.08406);
+        monoisotopicMasses.put("M", 131.04049);
+        monoisotopicMasses.put("N", 114.04293);
+        monoisotopicMasses.put("P", 97.05276);
+        monoisotopicMasses.put("Q", 128.05858);
+        monoisotopicMasses.put("R", 156.10111);
+        monoisotopicMasses.put("S", 87.03203);
+        monoisotopicMasses.put("T", 101.04768);
+        monoisotopicMasses.put("V", 99.06841);
+        monoisotopicMasses.put("W", 186.07931);
+        monoisotopicMasses.put("Y", 163.06333);
+    }
+
+    public static Double calculateProteinMass(final String proteinString) {
+        // in prod we should check that it's AUCG string and that the length is divisible by 3.
+        // we can also use stop sequences here, but it's not asked.
+        return Stream.of(proteinString.split(""))
+                .map(protein -> monoisotopicMasses.get(protein))
+                .mapToDouble(Double::doubleValue)
+                .sum();
+    }
+}
---
+++ alexander_aphonin/proteinStringFromRna/Task5.java
@@ -0,0 +1,86 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Task5 {
+    public static Map<String, String> proteinAbbreviations;
+    static {
+        proteinAbbreviations = new HashMap<>();
+        proteinAbbreviations.put("UUU", "F");
+        proteinAbbreviations.put("UUC", "F");
+        proteinAbbreviations.put("UUA", "L");
+        proteinAbbreviations.put("UUG", "L");
+        proteinAbbreviations.put("UCU", "S");
+        proteinAbbreviations.put("UCC", "S");
+        proteinAbbreviations.put("UCA", "S");
+        proteinAbbreviations.put("UCG", "S");
+        proteinAbbreviations.put("UAU", "Y");
+        proteinAbbreviations.put("UAC", "Y");
+        proteinAbbreviations.put("UAA", "");
+        proteinAbbreviations.put("UAG", "");
+        proteinAbbreviations.put("UGU", "C");
+        proteinAbbreviations.put("UGC", "C");
+        proteinAbbreviations.put("UGA", "");
+        proteinAbbreviations.put("UGG", "W");
+        proteinAbbreviations.put("CUU", "L");
+        proteinAbbreviations.put("CUC", "L");
+        proteinAbbreviations.put("CUA", "L");
+        proteinAbbreviations.put("CUG", "L");
+        proteinAbbreviations.put("CCU", "P");
+        proteinAbbreviations.put("CCC", "P");
+        proteinAbbreviations.put("CCA", "P");
+        proteinAbbreviations.put("CCG", "P");
+        proteinAbbreviations.put("CAU", "H");
+        proteinAbbreviations.put("CAC", "H");
+        proteinAbbreviations.put("CAA", "Q");
+        proteinAbbreviations.put("CAG", "Q");
+        proteinAbbreviations.put("CGU", "R");
+        proteinAbbreviations.put("CGC", "R");
+        proteinAbbreviations.put("CGA", "R");
+        proteinAbbreviations.put("CGG", "R");
+        proteinAbbreviations.put("AUU", "I");
+        proteinAbbreviations.put("AUC", "I");
+        proteinAbbreviations.put("AUA", "I");
+        proteinAbbreviations.put("AUG", "M");
+        proteinAbbreviations.put("ACU", "T");
+        proteinAbbreviations.put("ACC", "T");
+        proteinAbbreviations.put("ACA", "T");
+        proteinAbbreviations.put("ACG", "T");
+        proteinAbbreviations.put("AAU", "N");
+        proteinAbbreviations.put("AAC", "N");
+        proteinAbbreviations.put("AAA", "K");
+        proteinAbbreviations.put("AAG", "K");
+        proteinAbbreviations.put("AGU", "S");
+        proteinAbbreviations.put("AGC", "S");
+        proteinAbbreviations.put("AGA", "R");
+        proteinAbbreviations.put("AGG", "R");
+        proteinAbbreviations.put("GUU", "V");
+        proteinAbbreviations.put("GUC", "V");
+        proteinAbbreviations.put("GUA", "V");
+        proteinAbbreviations.put("GUG", "V");
+        proteinAbbreviations.put("GCU", "A");
+        proteinAbbreviations.put("GCC", "A");
+        proteinAbbreviations.put("GCA", "A");
+        proteinAbbreviations.put("GCG", "A");
+        proteinAbbreviations.put("GAU", "D");
+        proteinAbbreviations.put("GAC", "D");
+        proteinAbbreviations.put("GAA", "E");
+        proteinAbbreviations.put("GAG", "E");
+        proteinAbbreviations.put("GGU", "G");
+        proteinAbbreviations.put("GGC", "G");
+        proteinAbbreviations.put("GGA", "G");
+        proteinAbbreviations.put("GGG", "G");
+    }
+
+    public static String getProteinString(final String rnaString) {
+        // in prod we should check that it's AUCG string and that the length is divisible by 3.
+        // we can also use stop sequences here, but it's not asked.
+        return IntStream
+                .iterate(0, i -> i + 3)
+                .limit((int) Math.ceil(rnaString.length() / 3.0))
+                .mapToObj(i -> rnaString.substring(i, i+3))
+                .map(nuclTriplet -> proteinAbbreviations.get(nuclTriplet))
+                .collect(Collectors.joining());
+    }
+}
