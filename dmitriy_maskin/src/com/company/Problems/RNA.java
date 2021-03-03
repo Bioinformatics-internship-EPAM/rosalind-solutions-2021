77689b9cd975cf089c0b4a79a600df7e22ad028d
Dmitriy Maskin <36866382+maskindmitriy@users.noreply.github.com>
2021-03-03 04:56:22 +0300

    Delete dmitriy_maskin/src/com/company directory

--- dmitriy_maskin/src/com/company/Problems/RNA.java
+++
@@ -1,8 +0,0 @@
-package com.company.Problems;
-
-public class RNA {
-    //RNA	Transcribing DNA into RNA
-    public static String transcrib(String s) {
-        return s.replace('T', 'U');
-    }
-}
