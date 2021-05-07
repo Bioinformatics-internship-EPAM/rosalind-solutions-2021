fbbf9d16607727a0d9fedde16da431dee86a8818
astralromance <andrdoncwork@mail.ru>
2021-05-08 01:37:31 +0300

    Reworked protein calculation. Add mass calculating. Add report

--- andrey_dontsov/crs/main.py
+++ andrey_dontsov/crs/main.py
@@ -1,70 +1,118 @@
-# https://www.ncbi.nlm.nih.gov/nuccore/MT135041.1?report=fasta
+# https://www.ncbi.nlm.nih.gov/nuccore/MT027062.1?report=fasta
 
 CODON_TABLE = {
-    'UUU': 'F', 'CUU': 'L', 
-    'AUU': 'I', 'GUU': 'V',
-    'UUC': 'F', 'CUC': 'L',
-    'AUC': 'I', 'GUC': 'V',
-    'UUA': 'L', 'CUA': 'L',
-    'AUA': 'I', 'GUA': 'V',
-    'UUG': 'L', 'CUG': 'L',
-    'AUG': 'M', 'GUG': 'V',
-    'UCU': 'S', 'CCU': 'P',
-    'ACU': 'T', 'GCU': 'A',
-    'UCC': 'S', 'CCC': 'P',
-    'ACC': 'T', 'GCC': 'A',
-    'UCA': 'S', 'CCA': 'P',
-    'ACA': 'T', 'GCA': 'A',
-    'UCG': 'S', 'CCG': 'P',
-    'ACG': 'T', 'GCG': 'A',
-    'UAU': 'Y', 'CAU': 'H',
-    'AAU': 'N', 'GAU': 'D',
-    'UAC': 'Y', 'CAC': 'H',
-    'AAC': 'N', 'GAC': 'D',
-    'CAA': 'Q', 'AAA': 'K',
-    'GAA': 'E', 'CAG': 'Q',
-    'AAG': 'K', 'GAG': 'E',
-    'UGU': 'C', 'CGU': 'R',
-    'AGU': 'S', 'GGU': 'G',
-    'UGC': 'C', 'CGC': 'R',
-    'AGC': 'S', 'GGC': 'G',
-    'CGA': 'R', 'AGA': 'R',
-    'GGA': 'G', 'UGG': 'W',
-    'CGG': 'R', 'AGG': 'R',
-    'GGG': 'G' 
+    'TTT': 'F', 'CTT': 'L', 'ATT': 'I', 'GTT': 'V',
+    'TTC': 'F', 'CTC': 'L', 'ATC': 'I', 'GTC': 'V',
+    'TTA': 'L', 'CTA': 'L', 'ATA': 'I', 'GTA': 'V',
+    'TTG': 'L', 'CTG': 'L', 'ATG': 'M', 'GTG': 'V',
+    'TCT': 'S', 'CCT': 'P', 'ACT': 'T', 'GCT': 'A',
+    'TCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
+    'TCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
+    'TCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
+    'TAT': 'Y', 'CAT': 'H', 'AAT': 'N', 'GAT': 'D',
+    'TAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
+    'TAA': 'Stop', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
+    'TAG': 'Stop', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
+    'TGT': 'C', 'CGT': 'R', 'AGT': 'S', 'GGT': 'G',
+    'TGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
+    'TGA': 'Stop', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
+    'TGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
 }
 
-STOP_CODONES = ['UAA', 'UAG', 'UGA']
+MASS_TABLE = {
+    "A": 71.03711,
+    "C": 103.00919,
+    "D": 115.02694,
+    "E": 129.04259,
+    "F": 147.06841,
+    "G": 57.02146,
+    "H": 137.05891,
+    "I": 113.08406,
+    "K": 128.09496,
+    "L": 113.08406,
+    "M": 131.04049,
+    "N": 114.04293,
+    "P": 97.05276,
+    "Q": 128.05858,
+    "R": 156.10111,
+    "S": 87.03203,
+    "T": 101.04768,
+    "V": 99.06841,
+    "W": 186.07931,
+    "Y": 163.06333,
+}
+
+def reverse_complement(dna: str) -> str:
+    dict = {
+        'A': 'T', 'T': 'A',
+        'G': 'C', 'C': 'G'
+    }
+    res = []
+    for i in reversed(dna):
+        res.append(dict[i])
+    return ''.join(res)
+
+def map_codons(codon: str) -> str:
+    protein = None
+    if len(codon) == 3 and CODON_TABLE[codon]:
+        protein = CODON_TABLE[codon]
+    return protein
+
+def get_proteins(src_string: str) -> list:
+    res = []
+    protein_map = []
+
+    for index in range(len(src_string)):
+        protein = map_codons(src_string[index:index+3])
+        if protein and protein == 'M':
+            protein_map.append(index)
+
+    for index in protein_map:
+        is_stop = False
+        protein_str = ''
+
+        for i in range(index, len(src_string), 3):
+            protein = map_codons(src_string[i:i+3])
+
+            if not protein:
+                break
+            if protein == 'Stop':
+                is_stop = True
+                break
+            
+            protein_str += protein
+        if is_stop:
+            res.append(protein_str)
+    return res
+
+def mass_count(proteins: list) -> list:
+    result = []
+    for protein in proteins:
+        mass = 0
+        for s in protein:
+            if MASS_TABLE[s]:
+                mass += MASS_TABLE[s]
+        result.append({ protein: mass })
+    return result
 
 
-def dna_to_rna(dna_source: str) -> str:
-    return dna_source.replace('T', 'U')
+source_dna = ''
+with open('source_genom.txt', 'r') as src:
+    source_dna = ''.join([i.strip() for i in src.readlines()])
 
 
-def rna_to_protein(rna_source: str) -> str:
-    rna_separated = [rna_source[i: i+3] for i in range(0, len(rna_source), 3)]
-    protein = ''
-    if len(rna_separated[-1]) < 3:
-        rna_separated = rna_separated[:-1]
-    for cdn in rna_separated:
-        if cdn not in STOP_CODONES:
-            protein += CODON_TABLE[cdn]
-        else:
-            print(f'Found stop codone: {cdn}')
-            print(f'Current protein string: {protein}')
-            yield protein
-            protein = ''
+proteins = get_proteins(source_dna)
+with open('proteins.txt', 'w') as prt:
+    for protein in proteins:
+        prt.write(f'{protein}\n')
 
-def molecular_mass(proteins):
-    pass
+proteins_reversed = get_proteins(reverse_complement(source_dna))
+with open('proteins_complement.txt', 'w') as prt:
+    for protein in proteins_reversed:
+        prt.write(f'{protein}\n')
 
-genom_string = ''
-with open('source_genom.txt', 'r') as source_file:
-    source_strings = source_file.readlines()
-    genom_string = ''.join([i.replace('\n', '') for i in source_strings])
-
-
-with open('proteins.txt', 'w') as outp_file:
-    proteins = [i for i in rna_to_protein(dna_to_rna(genom_string))]
-    for i in proteins:
-        outp_file.write(i + '\n')
+proteins = set(proteins + proteins_reversed)
+mass = mass_count(proteins)
+with open('mass.txt', 'w') as prt:
+    for protein in mass:
+        prt.write(f'{protein.items()}\n')
